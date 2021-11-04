72134d6cdd8ab47686b7507df18117b477f0c2d7
Ryan Olf <ryan@nanoprecisionmedical.com>
2021-11-04 10:43:41 -0700

    Removed xml-rs dependency

--- examples/manager.rs
+++ examples/manager.rs
@@ -3,14 +3,16 @@ use std::time::Duration;
 
 use sonor::manager::{Manager, Error, MediaSource::*};
 use tokio::time::sleep;
+// const ZONE: &str = "Sonos Roam";
+const ZONE: &str = "Living Room";
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     simple_logger::init_with_level(log::Level::Debug).unwrap();
 
-    let manager = Manager::new_with_roomname("Living Room").await?;
+    let manager = Manager::new_with_roomname(ZONE).await?;
     println!("Got manager");
-    let zone = manager.get_zone("Living Room".into()).await?;
+    let zone = manager.get_zone(ZONE.into()).await?;
     // let snapshot = zone.take_snapshot().await?;
     // zone.clear_queue().await?;
     // zone.play_now(Apple("librarytrack:a.1442979904".into())).await?;
@@ -19,20 +21,20 @@ async fn main() -> Result<(), Error> {
     // zone.play_now(Spotify("track:4LI1ykYGFCcXPWkrpcU7hn".into())).await?;
     // zone.play_now(Spotify("album:4hW2wvP51Myt7UIVTgSp4f".into())).await?;
     // zone.play_now(Spotify("user:spotify:playlist:32O0SSXDNWDrMievPkV0Im".into())).await?;
-    // zone.play_now(Apple("album:1025210938".into())).await?;
+    // zone.play_now(Apple("album:1439398162".into())).await?;
     // zone.play_now(SonosFavorite("New York Rhapsody".into())).await?;
     // sleep(Duration::from_secs(10)).await;
     // zone.set_play_mode(sonor::RepeatMode::One, true).await?;
     // zone.play_or_pause().await?;
-    sleep(Duration::from_secs(5)).await;
+    // sleep(Duration::from_secs(5)).await;
     // zone.next_track().await?;
-    sleep(Duration::from_secs(5)).await;
+    // sleep(Duration::from_secs(5)).await;
     // zone.pause().await?;
     // zone.previous_track().await?;
     zone.set_shuffle(true).await?;
     // zone.seek_track(seconds)
     // zone.set_play_mode(sonor::RepeatMode::None, false).await?;
-    // zone.play_now(SonosPlaylist("Cars 1, 2, 3".into())).await?;
+    zone.play_now(SonosPlaylist("Cars 1, 2, 3".into())).await?;
     // zone.pause().await?;
     // zone.apply_snapshot(snapshot).await?;
     // zone.seek_rel_track(5).await?;
